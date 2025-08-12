aef536638eff42e8ec2197423383c0dc0315fe21
swapniltake1 <swapniltake1@outlook.com>
2025-08-12 21:39:04 +0530

    first commit

---
+++ src/main/java/com/expancetrackerapp/ExpanceTrackeraAppApplication.java
@@ -0,0 +1,13 @@
+package com.expancetrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpanceTrackeraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpanceTrackeraAppApplication.class, args);
+	}
+
+}
